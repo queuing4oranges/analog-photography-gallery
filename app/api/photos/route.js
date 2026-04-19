ea0147576e4cb48b5563bc5f715fa362787127d2
queuing4oranges <katjazenker@gmail.com>
2026-04-19 17:46:15 +0200

    comment currently not needed code

--- app/api/photos/route.js
+++ app/api/photos/route.js
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
-import { createClient } from "@/utils/supabase/server";
+// import { NextResponse } from "next/server";
+// import { createClient } from "@/utils/supabase/server";
 
-const API_BASE_URL = process.env.API_BASE_URL;
+// const API_BASE_URL = process.env.API_BASE_URL;
 
-const supabase = createRouteLoader;
+// const supabase = createRouteLoader;
 
 // export async function GET() {
 //     const res = await fetch(`${API_BASE_URL}/images/read.php`);
@@ -15,38 +15,38 @@ const supabase = createRouteLoader;
 //     })
 // }
 
-export async function POST(request) {
-  const photo = await request.json();
+// export async function POST(request) {
+//   const photo = await request.json();
 
-  // get supabase instanceconst cookieStore = await cookies()
-  const supabase = createClient(cookieStore);
+//   // get supabase instanceconst cookieStore = await cookies()
+//   const supabase = createClient(cookieStore);
 
-  // get the current user session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+//   // get the current user session
+//   const {
+//     data: { session },
+//   } = await supabase.auth.getSession();
 
-  // insert the data
-  //  TODO: refactor to upload photo
-  const { data, error } = await supabase
-    .from("photos")
-    .select() //'photos' = name of table
-    .insert({
-      ...photo,
-      // user_email: session.user.email //if available ?
-    });
+//   // insert the data
+//   //  TODO: refactor to upload photo
+//   const { data, error } = await supabase
+//     .from("photos")
+//     .select() //'photos' = name of table
+//     .insert({
+//       ...photo,
+//       // user_email: session.user.email //if available ?
+//     });
 
-  return NextResponse.json({ data, error });
+//   return NextResponse.json({ data, error });
 
-  // const res = await fetch(`${API_BASE_URL}/images/upload.php`,{
-  //     method: 'POST',
-  //     headers: {"Content-Type": "application/json"},
-  //     body: JSON.stringify(photo)
-  // });
+// const res = await fetch(`${API_BASE_URL}/images/upload.php`,{
+//     method: 'POST',
+//     headers: {"Content-Type": "application/json"},
+//     body: JSON.stringify(photo)
+// });
 
-  // const newPhoto = await res.json();
+// const newPhoto = await res.json();
 
-  // return NextResponse.json(newTicket, {
-  //     status: 201 //added new resource
-  // })
-}
+// return NextResponse.json(newTicket, {
+//     status: 201 //added new resource
+// })
+// }
