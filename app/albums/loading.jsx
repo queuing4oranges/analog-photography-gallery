4f8fd1ea38bfc1a3423787ce9989b1f119b02751
queuing4oranges <katjazenker@gmail.com>
2026-05-09 22:03:53 +0200

    add loader and styling

--- app/albums/loading.jsx
+++ app/albums/loading.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./loading.module.scss";
 
 export default function Loading() {
   return (
-    <main className="flex justify-center">
-      <h2>Loading...</h2>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className={styles.loader}></div>
+      <p className="text-accent-warm">&nbsp;... takes time</p>
     </main>
   );
 }
