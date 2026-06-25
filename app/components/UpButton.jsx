cd9383f32593846dbf27492400200d8a3080a6a1
queuing4oranges <katjazenker@gmail.com>
2026-06-25 22:02:58 +0200

    fix up button

--- app/components/UpButton.jsx
+++ app/components/UpButton.jsx
@@ -1,29 +1,51 @@
 "use client"
 
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function UpButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const detectScroll = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+
+    detectScroll();
+
+    window.addEventListener("scroll", detectScroll)
+    return () => window.removeEventListener("scroll", detectScroll)
+
+  }, [])
 
   return (
-    <button
-      className="absolute flex items-center justify-center bottom-5 right-5 mx-5 w-10 h-10 bg-transparent text-white border border-white rounded-full"
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })}
-      title='Up up up'
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+    <div>
+    {isVisible &&
+      <button
+        className="fixed flex items-center justify-center bottom-3 right-3 w-10 h-10 bg-transparent text-white border border-white rounded-full"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })}
+        title='Up up up'
       >
-        <path d="M12 19V5" />
-        <path d="m5 12 7-7 7 7" />
-      </svg>
-    </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M12 19V5" />
+          <path d="m5 12 7-7 7 7" />
+        </svg>
+      </button>
+    }
+
+    </div>
   )
 }
